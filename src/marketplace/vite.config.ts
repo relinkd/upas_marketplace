5392a4ee4d7701df57e1056dabd06468a2a0d588
Leontiy <leosungun@gmail.com>
2024-10-01 19:14:37 +0300

    deploy to mainnet

--- src/marketplace/vite.config.ts
+++ src/marketplace/vite.config.ts
@@ -1,52 +1,55 @@
 /// <reference types="vitest" />
 import svgr from 'vite-plugin-svgr';
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import EnvironmentPlugin from 'vite-plugin-environment';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4943',
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:4943',
+          changeOrigin: true,
+        },
       },
     },
-  },
-  build: {
-    outDir: 'build',
-  },
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-      }
-      
-    }),
-    EnvironmentPlugin('all', { prefix: 'CANISTER_' }),
-    EnvironmentPlugin('all', { prefix: 'DFX_' }),
-    EnvironmentPlugin(['CANISTER_ID_INDEXER', 'DFX_NETWORK'])
-  ],
-  resolve: {
-    alias: {
-        app: '/src/app',
-        entities: '/src/entities',
-        features: '/src/features',
-        pages: '/src/pages',
-        shared: '/src/shared',
-        widgets: '/src/widgets',
-        process: 'process/browser'
+    build: {
+      outDir: 'build',
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: {
+          icon: true,
+        }
+      }),
+      EnvironmentPlugin('all', { prefix: 'CANISTER_' }),
+      EnvironmentPlugin('all', { prefix: 'DFX_' }),
+      EnvironmentPlugin(['CANISTER_ID_INDEXER', 'DFX_NETWORK'])
+    ],
+    resolve: {
+      alias: {
+          app: '/src/app',
+          entities: '/src/entities',
+          features: '/src/features',
+          pages: '/src/pages',
+          shared: '/src/shared',
+          widgets: '/src/widgets',
+          process: 'process/browser'
       },
     },
-  },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'globalThis',
+        },
+      },
+    },
+    define: {
+      'process.env': env
+    },
+  }
 })
